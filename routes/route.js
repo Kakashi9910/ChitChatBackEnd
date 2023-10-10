20e7d22e9cc250497eb962c8f619e959a7b8e6c8
Palash <palashsharma359@gmail.com>
2023-10-10 17:39:35 +0530

    case handeling

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,7 @@
 import express  from "express";
 import { addUser } from "../controller/user-controller.js";
 import { getUsers } from "../controller/user-controller.js";
-import { newConversation } from "../controller/Conversation_Controller.js";
+import { newConversation } from "../controller/conversation_controller.js";
 import { getConversation } from "../controller/Conversation_Controller.js";
 import { newMessage,getMessage} from "../controller/message_controller.js";
 const route=express.Router();
